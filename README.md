7779294dd613bea9a4c95e8e3909961dbc8ac409
Carl Johnson <me@carlmjohnson.net>
2022-06-21 10:47:59 -0400

    Init

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # go-cli [![GoDoc](https://godoc.org/github.com/carlmjohnson/nfspampurge?status.svg)](https://godoc.org/github.com/carlmjohnson/nfspampurge) [![Go Report Card](https://goreportcard.com/badge/github.com/carlmjohnson/nfspampurge)](https://goreportcard.com/report/github.com/carlmjohnson/nfspampurge)
 
-Deletes all messages in Netlify's spam box
+Deletes all messages in Netlify's spam box.
+
+Usage: Go to Netlify's spam page. Remove any non-spam from the spam box. Go to the network
 
 ## Installation
 
@@ -15,4 +17,37 @@ GOBIN="$(pwd)" go install github.com/carlmjohnson/nfspampurge@latest
 ## Screenshots
 
 ```
+$ nfspampurge -h
+Netlify Spam Purge - devel
+
+Deletes all messages in Netlify's spam box.
+Options may be passed as env vars like NFSPAMPURGE_APP_ID.
+
+Get the cookie value by running this in the dev console:
+
+copy(JSON.parse(localStorage.getItem("nf-session")).access_token)
+
+Usage:
+
+        nfspampurge [options]
+
+Options:
+  -app-id id
+        id for Netlify app
+  -cookie _nf-auth
+        _nf-auth value for Netlify cookie
+  -form-id id
+        id for Netlify form
+  -silent
+        suppress logging
+  -timeout duration
+        timeout for connecting to Netlify (default 5s)
+
+$ nfspampurge
+nfspampurge 2022/06/21 10:36:37 got 55 entries
+nfspampurge 2022/06/21 10:36:37 purging 5de5d0d7cc558333db05a492 from Tue, 03 Dec 2019 03:04:55 UTC
+nfspampurge 2022/06/21 10:36:37 purging 5de4123fc25e8d9a36938999 from Sun, 01 Dec 2019 19:19:27 UTC
+nfspampurge 2022/06/21 10:36:37 purging 5de35d6eb4bbcd961eefa221 from Sun, 01 Dec 2019 06:27:58 UTC
+…
+nfspampurge 2022/06/21 10:36:51 got 0 entries
 ```
--- purge/purge.go
+++ purge/purge.go
@@ -1,18 +1,22 @@
 package purge
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	"io"
 	"log"
+	"net/http"
 	"os"
+	"os/signal"
+	"time"
 
 	"github.com/carlmjohnson/flagx"
-	"github.com/carlmjohnson/flagx/lazyio"
+	"github.com/carlmjohnson/requests"
 	"github.com/carlmjohnson/versioninfo"
 )
 
-const AppName = "Netlify Spam Purge"
+const AppName = "nfspampurge"
 
 func CLI(args []string) error {
 	var app appEnv
@@ -28,22 +32,34 @@ func CLI(args []string) error {
 
 func (app *appEnv) ParseArgs(args []string) error {
 	fl := flag.NewFlagSet(AppName, flag.ContinueOnError)
-	src := lazyio.FileOrURL(lazyio.StdIO, nil)
-	app.src = src
-	fl.Var(src, "src", "source file or URL")
-	app.Logger = log.New(io.Discard, AppName+" ", log.LstdFlags)
-	verbose := fl.Bool("verbose", false, "log debug output")
-	fl.Usage = func() {
-		fmt.Fprintf(fl.Output(), `nfspampurge - %s
 
-Deletes all messages in Netlify's spam box
+	app.Logger = log.New(os.Stderr, AppName+" ", log.LstdFlags|log.Lshortfile)
+	flagx.BoolFunc(fl, "silent", "suppress logging", func() error {
+		app.Logger.SetOutput(io.Discard)
+		return nil
+	})
+
+	appID := fl.String("app-id", "", "`id` for Netlify app")
+	formID := fl.String("form-id", "", "`id` for Netlify form")
+	cookie := fl.String("cookie", "", "`_nf-auth` value for Netlify cookie")
+	fl.DurationVar(&http.DefaultClient.Timeout, "timeout", 5*time.Second, "timeout for connecting to Netlify")
+
+	fl.Usage = func() {
+		fmt.Fprintf(fl.Output(), `Netlify Spam Purge - %s
+
+Deletes all messages in Netlify's spam box.
+Options may be passed as env vars like NFSPAMPURGE_APP_ID.
+
+Get the cookie value by running this in the dev console:
+
+copy(JSON.parse(localStorage.getItem("nf-session")).access_token)
 
 Usage:
 
 	nfspampurge [options]
 
 Options:
-`, versioninfo.Version)
+`, versioninfo.Short())
 		fl.PrintDefaults()
 	}
 	if err := fl.Parse(args); err != nil {
@@ -52,29 +68,113 @@ Options:
 	if err := flagx.ParseEnv(fl, AppName); err != nil {
 		return err
 	}
-	if *verbose {
-		app.Logger.SetOutput(os.Stderr)
+	if err := flagx.MustHave(fl, "app-id", "form-id", "cookie"); err != nil {
+		return err
 	}
+	app.cl = requests.
+		URL("https://app.netlify.com/").
+		Pathf(
+			"/access-control/bb-api/api/v1/sites/%s/forms/%s/submissions",
+			*appID, *formID,
+		).
+		Cookie("_nf-auth", *cookie)
 	return nil
 }
 
 type appEnv struct {
-	src io.ReadCloser
 	*log.Logger
+	cl *requests.Builder
 }
 
 func (app *appEnv) Exec() (err error) {
 	app.Println("starting")
-	defer func() { app.Println("done") }()
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer stop()
 
-	n, err := io.Copy(os.Stdout, app.src)
-	defer func() {
-		e2 := app.src.Close()
-		if err == nil {
-			err = e2
+	for {
+		entries, err := app.Get(ctx)
+		if err != nil {
+			return err
 		}
-	}()
-	app.Printf("copied %d bytes\n", n)
+		if len(entries) == 0 {
+			return nil
+		}
+		if err = app.Purge(ctx, entries); err != nil {
+			return err
+		}
+	}
+}
 
-	return err
+func (app *appEnv) Get(ctx context.Context) (entries NFResponse, err error) {
+	err = app.cl.Clone().
+		Param("state", "spam").
+		Param("page", "0").
+		Param("per_page", "100").
+		ToJSON(&entries).
+		Fetch(ctx)
+	app.Printf("got %d entries", len(entries))
+	return
+}
+
+func (app *appEnv) Purge(ctx context.Context, entries NFResponse) (err error) {
+	for _, entry := range entries {
+		id := entry.ID
+		app.Println("purging", id, "from", entry.CreatedAt.Format(time.RFC1123))
+		err = app.cl.Clone().
+			Pathf("/access-control/bb-api/api/v1/submissions/%s", id).
+			Delete().
+			CheckStatus(http.StatusNoContent).
+			Fetch(ctx)
+		if err != nil {
+			return err
+		}
+	}
+	return
+}
+
+type NFResponse []struct {
+	Number             int                  `json:"number"`
+	Title              any                  `json:"title"`
+	Email              string               `json:"email"`
+	Name               string               `json:"name"`
+	FirstName          string               `json:"first_name"`
+	LastName           any                  `json:"last_name"`
+	Company            any                  `json:"company"`
+	Summary            string               `json:"summary"`
+	Body               string               `json:"body"`
+	Data               Data                 `json:"data"`
+	CreatedAt          time.Time            `json:"created_at"`
+	HumanFields        HumanFields          `json:"human_fields"`
+	OrderedHumanFields []OrderedHumanFields `json:"ordered_human_fields"`
+	ID                 string               `json:"id"`
+	FormID             string               `json:"form_id"`
+	SiteURL            string               `json:"site_url"`
+	FormName           string               `json:"form_name"`
+}
+
+type Data struct {
+	HostPage  string `json:"host_page"`
+	Name      string `json:"name"`
+	Email     string `json:"email"`
+	Subject   string `json:"subject"`
+	Cc        string `json:"CC"`
+	Comment   string `json:"comment"`
+	IP        string `json:"ip"`
+	UserAgent string `json:"user_agent"`
+	Referrer  string `json:"referrer"`
+}
+
+type HumanFields struct {
+	HostURL string `json:"Host URL"`
+	From    string `json:"From"`
+	Email   string `json:"Email"`
+	Subject string `json:"Subject"`
+	Cc      string `json:"Cc"`
+	Comment string `json:"Comment"`
+}
+
+type OrderedHumanFields struct {
+	Title string `json:"title"`
+	Name  string `json:"name"`
+	Value string `json:"value"`
 }
